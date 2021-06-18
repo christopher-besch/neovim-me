41b800c0199e55776c1bb10872c65a48d8a1ecc5
Raafat Turki <raafat.turki@protonmail.com>
2021-06-18 17:55:07 +0300

    gitsigns configs

--- lua/configs.lua
+++ lua/configs.lua
@@ -1,7 +1,29 @@
 local C = {}
 
 function C.gitsigns()
-	require 'gitsigns'.setup()
+	require 'gitsigns'.setup {
+		signs = {
+			add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+			change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+			delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+			topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+			changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		},
+		numhl = false,
+		linehl = false,
+		keymaps = {},
+		watch_index = {
+			interval = 1000
+		},
+		current_line_blame = false,
+		current_line_blame_delay = 100,
+		current_line_blame_position = 'eol',
+		sign_priority = 6,
+		update_debounce = 100,
+		status_formatter = nil, -- Use default
+		use_decoration_api = true,
+		use_internal_diff = true,  -- If luajit is present
+	}
 end
 
 function C.colorizer()
